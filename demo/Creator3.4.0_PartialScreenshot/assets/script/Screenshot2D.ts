8512e65d6b09b1c214e4055f534711a8cc03055c
zhefeng.zhang <35944775+zhefengzhang@users.noreply.github.com>
2021-12-29 18:48:27 +0800

    upload

---
+++ demo/Creator3.4.0_PartialScreenshot/assets/script/Screenshot2D.ts
@@ -0,0 +1,224 @@
+
+import { _decorator, Component, Node, Camera, RenderTexture, director, gfx, ImageAsset, renderer, view, Size, Texture2D, SpriteFrame, Sprite, UITransform, spriteAssembler, sys, Vec2, Canvas, warnID, log, error, Button, assetManager, instantiate, Vec3, Label } from 'cc';
+import { JSB, PREVIEW } from 'cc/env';
+import { Canvas2Image } from "./Canvas2Image";
+const { ccclass, property } = _decorator;
+
+@ccclass('Screenshot2D')
+export class Screenshot2D extends Component {
+
+    @property(Camera)
+    copyCamera: Camera = null!;
+
+    @property(Node)
+    targetNode: Node = null!;
+
+    @property(Button)
+    downloaderBtn: Button = null!;
+
+    @property(Node)
+    copyNode: Node = null!;
+
+    @property(Label)
+    tips: Label = null!;
+
+    rt: RenderTexture = null;
+
+    _canvas: HTMLCanvasElement = null!;
+
+    _buffer: ArrayBufferView = null!;
+
+    canvas2image: Canvas2Image = null!;
+
+    start() {
+        this.canvas2image = Canvas2Image.getInstance();
+        this.rt = new RenderTexture();
+        this.rt.reset({
+            width: view.getVisibleSize().width,
+            height: view.getVisibleSize().height,
+        })
+        this.copyCamera.targetTexture = this.rt;
+        this.scheduleOnce(() => {
+            this.capture();
+        }, 2)
+    }
+
+    capture() {
+        this.copyRenderTex();
+    }
+
+    copyRenderTex() {
+        var width = this.targetNode.getComponent(UITransform).width;
+        var height = this.targetNode.getComponent(UITransform).height;
+        var worldPos = this.targetNode.getWorldPosition();
+        this._buffer = this.rt.readPixels(Math.round(worldPos.x), Math.round(worldPos.y), width, height);
+        this.showImage(width, height);
+    }
+
+    showImage(width, height) {
+        let img = new ImageAsset();
+        img.reset({
+            _data: this._buffer,
+            width: width,
+            height: height,
+            format: Texture2D.PixelFormat.RGBA8888,
+            _compressed: false
+        });
+        let texture = new Texture2D();
+        texture.image = img;
+        let sf = new SpriteFrame();
+        sf.texture = texture;
+        sf.packable = false;
+        this.copyNode!.getComponent(Sprite).spriteFrame = sf;
+        this.copyNode!.getComponent(Sprite).spriteFrame.flipUVY = true;
+        if (sys.isNative && (sys.os === sys.OS.IOS || sys.os === sys.OS.OSX)) {
+            this.copyNode!.getComponent(Sprite).spriteFrame.flipUVY = false;
+        }
+        this.copyNode?.getComponent(UITransform)?.setContentSize(new Size(width, height));
+        this.downloaderBtn.node.active = true;
+        this.tips.string = `截图成功`;
+    }
+
+    savaAsImage(width, height, arrayBuffer) {
+        if (sys.isBrowser) {
+            if (!this._canvas) {
+                this._canvas = document.createElement('canvas');
+                this._canvas.width = width;
+                this._canvas.height = height;
+            } else {
+                this.clearCanvas();
+            }
+            let ctx = this._canvas.getContext('2d')!;
+            let rowBytes = width * 4;
+            for (let row = 0; row < height; row++) {
+                let sRow = height - 1 - row;
+                let imageData = ctx.createImageData(width, 1);
+                let start = sRow * width * 4;
+                for (let i = 0; i < rowBytes; i++) {
+                    imageData.data[i] = arrayBuffer[start + i];
+                }
+                ctx.putImageData(imageData, 0, row);
+            }
+            //@ts-ignore
+            this.canvas2image.saveAsPNG(this._canvas, width, height);
+            this.tips.string = `保存图片成功`;
+        } else if (sys.isNative) {
+            // console.log("原生平台暂不支持图片下载");
+            // return;
+            let filePath = jsb.fileUtils.getWritablePath() + 'render_to_sprite_image.png';
+
+            // 目前 3.0.0 ~ 3.4.0 版本还不支持 jsb.saveImageData ,引擎计划在 3.5.0 支持, 要保存 imageData 为本地 png 文件需要参考下方的 pr 定制引擎
+            // https://gitee.com/zzf2019/engine-native/commit/1ddb6ec9627a8320cd3545d353d8861da33282a8
+
+            //@ts-ignore
+            if (jsb.saveImageData) {
+                //@ts-ignore
+                let success = jsb.saveImageData(this._buffer, width, height, filePath);
+                if (success) {
+                    // 用于测试图片是否正确保存到本地设备路径下
+                    assetManager.loadRemote<ImageAsset>(filePath, (err, imageAsset)=> {
+                        if (err) {
+                            console.log("show image error")
+                        } else {
+                            var newNode = instantiate(this.targetNode);
+                            newNode.setPosition(new Vec3(-newNode.position.x, newNode.position.y, newNode.position.z));
+                            this.targetNode.parent.addChild(newNode);
+                            
+                            const spriteFrame = new SpriteFrame();
+                            const texture = new Texture2D();
+                            texture.image = imageAsset;
+                            spriteFrame.texture = texture;
+                            newNode.getComponent(Sprite).spriteFrame = spriteFrame;
+                            spriteFrame.packable = false;
+                            spriteFrame.flipUVY = true;
+                            if (sys.isNative && (sys.os === sys.OS.IOS || sys.os === sys.OS.OSX)) {
+                                spriteFrame.flipUVY = false;
+                            }
+    
+                            this.tips.string = `成功保存在设备目录并加载成功: ${filePath}`;
+                        }
+                    });
+                    log("save image data success, file: " + filePath);
+                    this.tips.string = `成功保存在设备目录: ${filePath}`;
+                }
+                else {
+                    error("save image data failed!");
+                    this.tips.string = `保存图片失败`;
+                }
+            }
+        } else if (sys.platform === sys.Platform.WECHAT_GAME) {
+            if (!this._canvas) {
+                //@ts-ignore
+                this._canvas = wx.createCanvas();
+                this._canvas.width = width;
+                this._canvas.height = height;
+            } else {
+                this.clearCanvas();
+            }
+            var ctx = this._canvas.getContext('2d');
+
+            var rowBytes = width * 4;
+
+            for (var row = 0; row < height; row++) {
+                var sRow = height - 1 - row;
+                var imageData = ctx.createImageData(width, 1);
+                var start = sRow * width * 4;
+
+                for (var i = 0; i < rowBytes; i++) {
+                    imageData.data[i] = arrayBuffer[start + i];
+                }
+
+                ctx.putImageData(imageData, 0, row);
+            }
+            //@ts-ignore
+            this._canvas.toTempFilePath({
+                x: 0,
+                y: 0,
+                width: this._canvas.width,
+                height: this._canvas.height,
+                destWidth: this._canvas.width,
+                destHeight: this._canvas.height,
+                fileType: "png",
+                success: (res) =>{
+                    //@ts-ignore
+                    wx.showToast({
+                        title: "截图成功"
+                    });
+                    this.tips.string = `截图成功`;
+                    //@ts-ignore
+                    wx.saveImageToPhotosAlbum({
+                        filePath: res.tempFilePath,
+                        success: (res)=> {
+                            //@ts-ignore              
+                            wx.showToast({
+                                title: "成功保存到设备相册",
+                            });
+                            this.tips.string = `成功保存在设备目录: ${res.tempFilePath}`;
+                        },
+                        fail: ()=> {
+                            this.tips.string = `保存图片失败`;
+                        }
+                    })
+                },
+                fail: ()=> {
+                    //@ts-ignore
+                    wx.showToast({
+                        title: "截图失败"
+                    });
+                    this.tips.string = `截图失败`;
+                }
+            })
+        }
+    }
+
+    clearCanvas() {
+        let ctx = this._canvas.getContext('2d');
+        ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    }
+
+    onSaveImageBtnClicked() {
+        var width = this.targetNode.getComponent(UITransform).width;
+        var height = this.targetNode.getComponent(UITransform).height;
+        this.savaAsImage(width, height, this._buffer)
+    }
+}
