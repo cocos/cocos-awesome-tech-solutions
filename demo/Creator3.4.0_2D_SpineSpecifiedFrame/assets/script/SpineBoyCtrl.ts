569ff66f8dfd10d7648d521a1098d78661329397
muxiandong <24754593@qq.com>
2022-01-07 01:32:40 +0800

    update

--- demo/Creator3.4.0_2D_SpineSpecifiedFrame/assets/script/SpineBoyCtrl.ts
+++ demo/Creator3.4.0_2D_SpineSpecifiedFrame/assets/script/SpineBoyCtrl.ts
@@ -1,62 +1,55 @@
-import { _decorator, Component, sp } from "cc";
+import { _decorator, Component, sp, AnimationState } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass('SpineBoyCtrl')
 export default class SpineBoyCtrl extends Component{
-
-    mixTime:number= 0.2;
-
     private spine !: sp.Skeleton;
-    private _hasStop = true;
+
+    frames : number = 0;
+    duration : number = 0;
+    frameRate : number = 30;
+    secondPerFrame : number = 0;
+
+    beginTime : number = 0;
+    endTime : number = 0;
+
+    timeCount : number = 0;
 
     onLoad () {
-        var spine = this.spine = this.getComponent('sp.Skeleton') as sp.Skeleton;
-        // this._setMix('walk', 'run');
-        // this._setMix('run', 'jump');
-        // this._setMix('walk', 'jump');
-
-        // spine.setStartListener(trackEntry => {
-        //     var animationName = trackEntry.animation ? trackEntry.animation.name : "";
-        //     console.log("[track %s][animation %s] start.", trackEntry.trackIndex, animationName);
-        // });
-        // spine.setInterruptListener(trackEntry => {
-        //     var animationName = trackEntry.animation ? trackEntry.animation.name : "";
-        //     console.log("[track %s][animation %s] interrupt.", trackEntry.trackIndex, animationName);
-        // });
-        // spine.setEndListener(trackEntry => {
-        //     var animationName = trackEntry.animation ? trackEntry.animation.name : "";
-        //     console.log("[track %s][animation %s] end.", trackEntry.trackIndex, animationName);
-        // });
-        // spine.setDisposeListener(trackEntry => {
-        //     var animationName = trackEntry.animation ? trackEntry.animation.name : "";
-        //     console.log("[track %s][animation %s] will be disposed.", trackEntry.trackIndex, animationName);
-        // });
-        // spine.setCompleteListener((trackEntry) => {
-        //     var animationName = trackEntry.animation ? trackEntry.animation.name : "";
-        //     if (animationName === 'shoot') {
-        //         this.spine!.clearTrack(1);
-        //     }
-        //     var loopCount = Math.floor(trackEntry.trackTime / trackEntry.animationEnd);
-        //     console.log("[track %s][animation %s] complete: %s", trackEntry.trackIndex, animationName, loopCount);
-        // });
-        // spine.setEventListener(((trackEntry:any, event:any) => {
-        //     var animationName = trackEntry.animation ? trackEntry.animation.name : "";
-        //     console.log("[track %s][animation %s] event: %s, %s, %s, %s", trackEntry.trackIndex, animationName, event.data.name, event.intValue, event.floatValue, event.stringValue);
-        // }) as any);
-
-        // this._hasStop = false;
-
-
-        console.log(this.spine);
-
-        console.log(this.spine.attachUtil);
-
+        this.spine = this.getComponent('sp.Skeleton') as sp.Skeleton;
         console.log(this.spine.getCurrent(0));
-        this.spine.getCurrent(0);
+
+        this.duration = this.spine.getCurrent(0).animation.duration;
+        this.frames = Math.ceil(this.frameRate * this.duration);
+        this.secondPerFrame = 1 / this.frameRate;
+
+        console.log(this.duration, this.frameRate, this.frames, this.secondPerFrame);
     }
 
-    // _setMix (anim1: string, anim2: string) {
-    //     this.spine.setMix(anim1, anim2, this.mixTime);
-    //     this.spine.setMix(anim2, anim1, this.mixTime);
-    // }
+    update (dt: number) {
+    }
+
+    onClick () {
+        this.spine.setAnimation(0, 'portal', false);
+
+        this.beginTime = 1.5;
+        this.endTime = 2.5;
+        this.doPlay();
+    }
+
+    // 指定时间播放
+    doPlay () {
+        this.spine.timeScale = 0;
+        this.spine.getCurrent(0).animationStart = this.beginTime;
+        this.spine.getCurrent(0).animationEnd = this.endTime;
+        this.spine.timeScale = 1;
+    }
+
+    // 指定时间播放(逆向)
+    doReverse () {
+        this.spine.timeScale = 0;
+        this.spine.getCurrent(0).animationStart = this.beginTime;
+        this.spine.getCurrent(0).animationEnd = this.endTime;
+        this.spine.timeScale = -1;
+    }
 }
