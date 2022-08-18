b8edfcae39c1c8861847a68a69d8f7798c41491d
zhefeng.zhang <337031709@qq.com>
2022-08-18 18:57:57 +0800

    Modify camera to reduce 3D screen shake, add 'W', 'A', ’S', 'D' button camera control

--- shader/Creator3.5.2_Snapshot_Shader/assets/Scenes/Scripts/CameraMove.ts
+++ shader/Creator3.5.2_Snapshot_Shader/assets/Scenes/Scripts/CameraMove.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Vec2, RigidBody, Vec3, Input, KeyCode } from 'cc';
+import { _decorator, Component, Node, Vec2, RigidBody, Vec3, Input, KeyCode, systemEvent, SystemEvent, macro, math } from 'cc';
 const { ccclass, property } = _decorator;
 
 import { InputEx } from './InputEx';
@@ -13,40 +13,103 @@ export class CameraMove extends Component {
 
     rigidbody: RigidBody = null;
 
+    private startShift = false;
+    private startW = false;
+    private startA = false;
+    private startS = false;
+    private startD = false;
+
     start() {
         InputEx.RegisterEvent();
         this.rigidbody = this.getComponent(RigidBody);
+
+        systemEvent.on(SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        systemEvent.on(SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+
+        //鼠标监听
+        systemEvent.on(SystemEvent.EventType.MOUSE_MOVE, this.onMouseMove, this);
     }
 
     onDestroy() {
         InputEx.UnRegisterEvent();
     }
 
-    update(dt: number) {
-        return;
-
-        // Rotate the camera.
-        this.rotation = this.rotation.add(new Vec3(-InputEx.GetAxis("Mouse Y"), InputEx.GetAxis("Mouse X"), 0));
-        this.node.setRotationFromEuler(this.rotation.multiplyScalar(this.cameraSpeed));
-
-        // Move the camera.
-        let x = InputEx.GetAxis("Horizontal");
-        let z = InputEx.GetAxis("Vertical");
-
-        let move: Vec3 = Vec3.ZERO;
-        let speed = this.moveSpeed;
-
-        move = new Vec3(x, 0.0, z);
-        speed = this.moveSpeed * (InputEx.GetKeyDown(KeyCode.SPACE) ? 2.5 : 1.0);
-
-        this.moveVector = move.multiplyScalar(speed);
+    onKeyUp(event) {
+        switch (event.keyCode) {
+            case macro.KEY.shift:
+                this.startShift = false;
+                break;
+            case macro.KEY.w:
+                this.startW = false;
+                break;
+            case macro.KEY.a:
+                this.startA = false;
+                break;
+            case macro.KEY.s:
+                this.startS = false;
+                break;
+            case macro.KEY.d:
+                this.startD = false;
+                break;
+        }
     }
 
-    lateUpdate() {
-        return;
-        
-        let wdir = this.moveVector.transformMat4(this.node.getWorldMatrix());
-        this.rigidbody.setLinearVelocity(wdir);
+    onKeyDown(event) {
+        switch (event.keyCode) {
+            case macro.KEY.shift:
+                this.startShift = true;
+                break;
+            case macro.KEY.w:
+                this.startW = true;
+                break;
+            case macro.KEY.a:
+                this.startA = true;
+                break;
+            case macro.KEY.s:
+                this.startS = true;
+                break;
+            case macro.KEY.d:
+                this.startD = true;
+                break;
+        }
+    }
+
+    onMouseMove(event) {
+        if (event.movementX != 0 && this.startShift) {
+            const up = new Vec3(0, 1, 0);
+            const right = new Vec3(1, 0, 0);
+            const rotationx = this.node.getRotation();
+            math.Quat.rotateAround(rotationx, rotationx, up, -event.movementX / 5 / 360.0 * 3.1415926535);
+            math.Quat.rotateAround(rotationx, rotationx, right, -event.movementY / 2.5 / 360.0 * 3.1415926535);
+            this.node.setRotation(rotationx);
+        }
+    }
+
+    update(deltaTime: number) {
+
+        if (this.startW) {
+
+            this.node.translate(new Vec3(0, deltaTime, 0));
+        }
+
+        if (this.startA) {
+
+            this.node.translate(new Vec3(-deltaTime, 0, 0));
+
+        }
+
+        if (this.startS) {
+
+            this.node.translate(new Vec3(0, -deltaTime, 0));
+
+        }
+
+        if (this.startD) {
+
+            this.node.translate(new Vec3(deltaTime, 0, 0));
+
+        }
+
     }
 }
 
